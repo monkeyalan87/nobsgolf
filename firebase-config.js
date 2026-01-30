164e301c66ac2cbaed525842fa0b0f753a44b83c
monkeyalan87 <161203265+monkeyalan87@users.noreply.github.com>
2026-01-30 18:55:54 +0000

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,18 +1,9 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwIICelwEDM58i3UQ7X8oK1CEorcpJ67U",
-  authDomain: "nobsgolf.firebaseapp.com",
-  databaseURL: "https://nobsgolf-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "nobsgolf",
-  storageBucket: "nobsgolf.firebasestorage.app",
-  messagingSenderId: "339004361968",
-  appId: "1:339004361968:web:9aa703f6d4fe6fb8a94635"
+  apiKey: "AIzaSyC1234567890abcdefg...",        // ← This is REAL
+  authDomain: "your-project.firebaseapp.com",
+  databaseURL: "https://your-project-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "your-project-id",
+  storageBucket: "your-project.appspot.com",
+  messagingSenderId: "123456789012",
+  appId: "1:123456:web:abcdef"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
